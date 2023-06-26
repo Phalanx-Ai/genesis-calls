25e1b5179efcae7aee53a25a81033799bc9511f4
Marek Grac <marek@bedeep.eu>
2023-06-26 12:28:20 +0200

    [feat] Version that setup three tables with conversation/agent/wrap_up_code

--- src/component.py
+++ src/component.py
@@ -1,21 +1,19 @@
-'''
-Template Component main class.
-
-'''
 import csv
 import logging
-from datetime import datetime
+import datetime
+import sys
 
 from keboola.component.base import ComponentBase
 from keboola.component.exceptions import UserException
 
-# configuration variables
-KEY_API_TOKEN = '#api_token'
-KEY_PRINT_HELLO = 'print_hello'
+import PureCloudPlatformClientV2 as pc2
+from PureCloudPlatformClientV2.rest import ApiException
 
-# list of mandatory parameters => if some is missing,
-# component will fail with readable message on initialization.
-REQUIRED_PARAMETERS = [KEY_PRINT_HELLO]
+KEY_CLIENT_ID = 'client_id'
+KEY_PASSWORD = '#password'
+KEY_CLOUD_URL = 'cloud_url'
+
+REQUIRED_PARAMETERS = [KEY_CLIENT_ID, KEY_PASSWORD, KEY_CLOUD_URL]
 REQUIRED_IMAGE_PARS = []
 
 
@@ -34,48 +32,104 @@ class Component(ComponentBase):
         super().__init__()
 
     def run(self):
-        '''
-        Main execution code
-        '''
+        DAYS_COUNT = 3
 
-        # ####### EXAMPLE TO REMOVE
-        # check for missing configuration parameters
         self.validate_configuration_parameters(REQUIRED_PARAMETERS)
         self.validate_image_parameters(REQUIRED_IMAGE_PARS)
         params = self.configuration.parameters
-        # Access parameters in data/config.json
-        if params.get(KEY_PRINT_HELLO):
-            logging.info("Hello World")
 
-        # get last state data/in/state.json from previous run
-        previous_state = self.get_state_file()
-        logging.info(previous_state.get('some_state_parameter'))
+        ## get data from previous calendar day only
+        start_date = datetime.datetime.combine(datetime.datetime.utcnow(), datetime.time(00, 00, 00)) - datetime.timedelta(days = DAYS_COUNT)
+        end_date = start_date + datetime.timedelta(days = DAYS_COUNT)
+        filter = {
+            "interval": "%s/%s" % (
+                start_date.isoformat(timespec="seconds"),
+                end_date.isoformat(timespec="seconds")
+            )
+        }
 
-        # Create output table (Tabledefinition - just metadata)
-        table = self.create_out_table_definition('output.csv', incremental=True, primary_key=['timestamp'])
+        ## obtain data
+        api_client = pc2.api_client.ApiClient(host=params.get(KEY_CLOUD_URL)).get_client_credentials_token(params.get(KEY_CLIENT_ID), params.get(KEY_PASSWORD))
 
-        # get file path of the table (data/out/tables/Features.csv)
-        out_table_path = table.full_path
-        logging.info(out_table_path)
+        conversation_api = pc2.ConversationsApi(api_client=api_client)
+        users_api = pc2.UsersApi(api_client=api_client)
+        routing_api = pc2.RoutingApi(api_client=api_client)
 
-        # DO whatever and save into out_table_path
-        with open(table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
-            writer = csv.DictWriter(out_file, fieldnames=['timestamp'])
+        output = {
+            "conversations": []
+        }
+        responses = conversation_api.post_analytics_conversations_details_query(filter)
+
+        if responses.conversations is None:
+            # @todo: prepare empty tables
+            sys.exit(0)
+
+        for conversation in responses.conversations:
+            c = {}
+
+            c['wrap_up_code'] = []
+            c['agents'] = []
+
+            c['conversation_id'] = conversation.conversation_id
+            c['conversation_start'] = conversation.conversation_start.isoformat(timespec="seconds")
+            c['conversation_end'] = conversation.conversation_end.isoformat(timespec="seconds")
+
+            ## Get wrap_up_code and decode it to text value
+            for p in conversation.participants:
+                for session in p.sessions:
+                    for segment in session.segments:
+                        if segment.wrap_up_code is not None:
+                            code_id = segment.wrap_up_code
+
+                            try:
+                                x = routing_api.get_routing_wrapupcode(code_id)
+                                c['wrap_up_code'].append(x.name)
+                            except:
+                                c['wrap_up_code'].append(code_id)
+
+            ## Get agents and their emails
+            if p.purpose == "agent" and p.user_id is not None:
+                c['agents'].append(users_api.get_user(p.user_id).username)
+
+            output['conversations'].append(c)
+
+        # Create output table - conversations
+        conversation_table = self.create_out_table_definition('conversations.csv', incremental=True, primary_key=['conversation_id'])
+        with open(conversation_table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
+            writer = csv.DictWriter(out_file, fieldnames=['conversation_id', 'conversation_start', 'conversation_end'])
             writer.writeheader()
-            writer.writerow({"timestamp": datetime.now().isoformat()})
+            for line in output['conversations']:
+                writer.writerow({key: value for key, value in line.items() if key not in ['agents', 'wrap_up_code'] })
+        self.write_manifest(conversation_table)
 
-        # Save table manifest (output.csv.manifest) from the tabledefinition
-        self.write_manifest(table)
+        # Create output table - agents
+        agents_table = self.create_out_table_definition('agents.csv', incremental=True, primary_key=['conversation_id', 'agent_email'])
+        with open(agents_table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
+            writer = csv.DictWriter(out_file, fieldnames=['conversation_id', 'agent_email'])
+            writer.writeheader()
+            for line in output['conversations']:
+                for agent in line['agents']:
+                    writer.writerow({
+                        'conversation_id' : line['conversation_id'],
+                        'agent_email': agent
+                    })
+        self.write_manifest(agents_table)
 
-        # Write new state - will be available next run
-        self.write_state_file({"some_state_parameter": "value"})
+        # Create output table - wrap up code
+        wrap_table = self.create_out_table_definition('wrap_up_code.csv', incremental=True, primary_key=['conversation_id', 'wrap_up_code'])
+        with open(wrap_table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
+            writer = csv.DictWriter(out_file, fieldnames=['conversation_id', 'wrap_up_code'])
+            writer.writeheader()
+            for line in output['conversations']:
+                for code in line['wrap_up_code']:
+                    writer.writerow({
+                        'conversation_id' : line['conversation_id'],
+                        'wrap_up_code' : code
+                    })
+        self.write_manifest(wrap_table)
 
-        # ####### EXAMPLE TO REMOVE END
 
 
-"""
-        Main entrypoint
-"""
 if __name__ == "__main__":
     try:
         comp = Component()
